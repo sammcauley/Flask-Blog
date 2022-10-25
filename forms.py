566b057c0bc0f446af8a9ceb28ad8e293b26b540
Sam McAuley <se.mcauley1@gmail.com>
2022-10-25 20:34:12 +0100

    Remove URL import

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired
 from flask_ckeditor import CKEditorField
 
 ##WTForm
